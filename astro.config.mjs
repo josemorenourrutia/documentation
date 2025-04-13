10c7ce54d4f88d364181ceb198543cecf89128fa
jose.matias.ramirez <jose.matias.ramirez@upc.edu>
2025-04-13 09:15:44 +0000

    ultimos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,13 +16,17 @@ export default defineConfig({
     // Set English as the default language for this site.
     defaultLocale: 'es',
     locales: {
-      // English docs in `src/content/docs/en/`
-      en: {
-        label: 'English',
-      },
       // Español docs in `src/content/docs/es/`
       es: {
         label: 'Español',
+      },
+      // English docs in `src/content/docs/en/`
+      ca: {
+        label: 'Catalán',
+      },
+      // English docs in `src/content/docs/en/`
+      en: {
+        label: 'Inglés',
       },
     },
   })]
